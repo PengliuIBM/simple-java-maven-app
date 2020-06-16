f39f6f57a43b9642936a16f0b7d3d145ffa53b43
datangliupeng@163.com <datangliupeng@163.com>
2020-06-16 12:19:33 +0800

    rollback
    rollback

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -6,7 +6,7 @@ package com.mycompany.app;
 public class App
 {
 
-    private final String message = "Hello World - Patrick Liu!";
+    private final String message = "Hello World!";
 
     public App() {}
 
