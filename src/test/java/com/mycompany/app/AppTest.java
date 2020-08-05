98183880f64c33c65f3fb77a0c9ab9187b707306
datangliupeng@163.com <datangliupeng@163.com>
2020-08-05 11:00:07 +0800

    test jenkins

--- src/test/java/com/mycompany/app/AppTest.java
+++ src/test/java/com/mycompany/app/AppTest.java
@@ -34,7 +34,7 @@ public class AppTest
     {
         App.main(null);
         try {
-            assertEquals("Hello World-Patrick!" + System.getProperty("line.separator"), outContent.toString());
+            assertEquals("Hello World-Patrick!123" + System.getProperty("line.separator"), outContent.toString());
         } catch (AssertionError e) {
             fail("\"message\" is not \"Hello World!\"");
         }
